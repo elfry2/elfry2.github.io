de17cb00f529178d3b531e1448340bb8254cab40
Elfry <elfry3@gmail.com>
2026-05-09 22:44:34 +0700

    Clean up and scaffold

---
+++ src/About.jsx
@@ -0,0 +1,115 @@
+export default {
+  nickname: "Elfry",
+  fullName: "Muhammad Elfri Rahmad Juang",
+  bio: "I make web apps, but I might also enjoy building your office network.",
+  available: true,
+  skills: [
+    "React",
+    "Laravel",
+    "PostgreSQL",
+    "MongoDB",
+    "Cisco",
+    "MikroTik"
+  ],
+  contacts: [
+    {
+      label: "elfry3@gmail.com",
+      materialIcon: "envelope",
+      url: "mailto:elfry3@gmail.com"
+    },
+    {
+      label: "github.com/elfry2",
+      materialIcon: "GitHub",
+      url: "https://github.com/elfry2"
+    },
+    {
+      label: "linkedin.com/in/elfry2",
+      materialIcon: "LinkedIn",
+      url: "https://linkedin.com/in/elfry2"
+    }
+  ],
+  projects: [
+    {
+      name: "BTW-ERP",
+      startDate: "2025",
+      description: "An ERP application tailored to the unique business needs of PT Berkah Tirta Wahana",
+      skills: ["React", "Laravel", "PostgreSQL"],
+      image: "https://i.postimg.cc/wxFyqjgh/image-2026-05-09-113634.png"
+    },
+    {
+      name: "dekita",
+      startDate: "2023",
+      description: "An multi-list to-do-list application built on Laravel 10, which I use as the template for my other Laravel apps at the time",
+      skills: ["Laravel", "MySQL", "Bootstrap"],
+      image: "https://i.postimg.cc/44w7fxsp/373943175-c1d5fe8e-7e4d-4848-98aa-ba72bf0bdc2a.png"
+    },
+    {
+      name: "depresi",
+      startDate: "2023",
+      description: "An Expert system for depression screening using the naive bayes algorithm",
+      skills: ["Laravel", "MySQL", "Bootstrap"],
+      image: "https://i.postimg.cc/gc4Xz2m8/224542705-9163a030-b322-4709-86da-3b108c12fa16.png"
+    },
+    {
+      name: "Cyberpsychology Paper Submission & Acceptance System",
+      startDate: "2023",
+      description: "A paper submission and acceptance app built for an international webinar event of hosted by five universities with speaker from Coventry University, UK",
+      skills: ["Laravel", "MySQL", "Bootstrap"],
+      image: null
+    },
+    {
+      name: "StockOpname",
+      startDate: "2022",
+      endDate: "",
+      description: "An application to record the in-and-outs of supplies in BPJS Ketenagakerjaan Panam",
+      skills: ["Laravel", "MySQL", "Bootstrap"],
+      image: null
+    },
+    {
+      name: "Persediaan",
+      startDate: "2021",
+      endDate: "",
+      description: "An application to record the in-and-outs of supplies in Kantor Imigrasi Kelas I TPI Pekanbaru",
+      skills: ["Laravel", "MySQL", "Bootstrap"],
+      image: null
+    }
+  ],
+  employments: [
+    {
+      name: "PT Berkah Tirta Wahana, Full-Stack Web Developer",
+      startDate: "2021",
+      endDate: "",
+      description: "Led a team of developers as the project manager in the development of BTW-ERP, an enterprise resource planning application built on top of Laravel and React to support workflows across departments including those previously handled by SISFO, customer chat management and late-reply warning system, etc.",
+      tags: ["React", "Laravel", "PostgreSQL"],
+      image: null
+    },
+    {
+      name: "PT Global Jet Express (J&T Express), Network Management Staff",
+      year: "Apr 2024–May 2025",
+      description: "Responsible for providing tactical support for branches in meeting diverse, fast-evolving KPIs, working with excellent and experienced Coordinators across major cities as well as remote districts in Riau. Made sure 70% of packets arrive before lunchtime",
+      tags: ["Operational Support", "Internal audit"],
+      image: null
+    },
+    {
+      name: "BPJS Ketenagakerjaan Panam, Full-Stack Web Developer Intern",
+      year: "Feb–Mar 2022",
+      description: "Responsible for the development of StockOpname: a web-based application to simplify the recording and the reporting of the in-and-outs of supplies",
+      tags: ["Laravel", "MySQL", "Bootstrap"],
+      image: null
+    },
+    {
+      name: "Kantor Imigrasi Kelas I TPI Pekanbaru, Full-Stack Web Developer Intern",
+      year: "Mar–Apr 2021",
+      description: "Responsible for the development of StockOpname: a web-based application to simplify the recording and the reporting of the in-and-outs of supplies",
+      tags: ["Laravel", "MySQL", "Bootstrap"],
+      image: null
+    },
+    {
+      name: "PT Aplikanusa Lintasarta, Network Engineer Intern",
+      year: "Jul–Oct 2017",
+      description: "Participated in multiple network installations and maintenance for numerous enterprises and public facilities across Pekanbaru including Sultan Syarif Kasim II International Airport, getting the opportunity of working with long-range routers & VSATs",
+      tags: ["Network installation", "Network troubleshooting", "Network maintenance"],
+      image: null
+    }
+  ]
+};
